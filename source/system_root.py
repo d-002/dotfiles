a2cb6fd3b683f70d07c7a4af58c6dc84fe9dd6c4
d-002 <minecraftenjoy@outlook.fr>
2026-04-05 00:07:07 +0200

    fstab, grub: symlinks rather than file copies

--- source/system_root.py
+++ source/system_root.py
@@ -4,7 +4,5 @@ import decman
 
 from common import HOME
 
-decman.files['/etc/default/grub'] = File(
-        source_file=f'{HOME}/dotfiles/system_root/grub')
-decman.files['/etc/fstab'] = File(
-        source_file=f'{HOME}/dotfiles/system_root/fstab')
+decman.symlinks['/etc/default/grub'] = f'{HOME}/dotfiles/system_root/grub'
+decman.symlinks['/etc/fstab'] = f'{HOME}/dotfiles/system_root/fstab'
