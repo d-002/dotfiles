05c07752932b9bb6077fc48a154632c226a501ea
d-002 <minecraftenjoy@outlook.fr>
2026-04-05 00:03:30 +0200

    greeter: added nwg-hello background image

--- source/greeter.py
+++ source/greeter.py
@@ -6,5 +6,7 @@ from common import HOME
 for name in ['config.toml', 'greetd.conf', 'sway-config']:
     decman.files[f'/etc/greetd/{name}'] = File(
             source_file=f'{HOME}/dotfiles/greeter/greetd/{name}')
-decman.directories[f'/etc/nwg-hello/custom'] = Directory(
+decman.directories['/etc/nwg-hello/custom'] = Directory(
         source_directory=f'{HOME}/dotfiles/greeter/nwg-hello/custom')
+decman.files['/usr/share/nwg-hello/clea.png'] = File(
+        source_file=f'{HOME}/dotfiles/greeter//nwg-hello/clea.png')
