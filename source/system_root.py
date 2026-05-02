8cd7da82b54caaebdf3c42ba8e35c790d0d35632
d-002 <mailofd00@gmail.com>
2026-05-02 19:22:29 +0200

    grub: added nvidia-drm

--- source/system_root.py
+++ source/system_root.py
@@ -1,6 +1,9 @@
 import decman
+from decman import File
 
 from common import HOME
 
-decman.symlinks['/etc/default/grub'] = f'{HOME}/dotfiles/system_root/grub'
-decman.symlinks['/etc/fstab'] = f'{HOME}/dotfiles/system_root/fstab'
+decman.files['/etc/default/grub'] = File(
+        source_file=f'{HOME}/dotfiles/system_root/grub')
+decman.files['/etc/fstab'] = File(
+        source_file=f'{HOME}/dotfiles/system_root/fstab')
