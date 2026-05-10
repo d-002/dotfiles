fd96edc3b5b1e41e32dbf23816855ccd9ac4d96a
d-002 <mailofd00@gmail.com>
2026-05-10 16:30:45 +0200

    fstab: removed

--- source/system_root.py
+++ source/system_root.py
@@ -5,5 +5,3 @@ from common import HOME
 
 decman.files['/etc/default/grub'] = File(
         source_file=f'{HOME}/dotfiles/system_root/grub')
-decman.files['/etc/fstab'] = File(
-        source_file=f'{HOME}/dotfiles/system_root/fstab')
